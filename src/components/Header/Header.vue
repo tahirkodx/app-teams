6a99ff5013e1907614402fff5f153d7faff5683c
adilameeqKOD <adilameeq@kodxsystem.com>
2023-12-20 16:12:20 +0500

    feat: header

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,184 +1,150 @@
 <template>
-  <!--div v-show="userStore.settings.showScores"-->
-  <ion-grid>
-    <ion-row>
-      <ion-col size="10">
-        <ion-title>
-          <ion-select
-            v-model="teamSelected"
-            interface="action-sheet"
-            @ion-change="changeTeam"
-          >
-            <div v-for="(id_team_tuple, index) in teamStore.teams.value" v-bind:key="index">
-              <ion-select-option :value="id_team_tuple[0]">{{
-                id_team_tuple[1].name
-              }}</ion-select-option>
-            </div>
-          </ion-select>
-        </ion-title>
-      </ion-col>
-      <ion-col size="2">
-        <ion-icon
-          :icon="menuOutline"
-          @click="() => router.push({ name: 'settings' })"
-          size="large"
-        ></ion-icon>
-      </ion-col>
-    </ion-row>
-    <div
-      v-show="
-        teamStore.isTeamlead(teamID, userStore.user) &&
-        statusStore.teamHasDimScores
-      "
-    >
-      <ion-row>
-        <ion-col id="level-indicator">
-          <div style="float: left">
-            <ion-icon :icon="prismOutline" size="large"></ion-icon>
-          </div>
-          <div style="font-size: 20px; float: left">
-            <div>
-              Level {{ Math.floor(statusStore.teamCurrentScore(teamID) / 10) }}
-            </div>
-            <ion-progress-bar
-              style="width: 100%"
-              :buffer="1"
-              :value="
-                statusStore.teamCurrentScore(teamID) / 10 -
-                Math.floor(statusStore.teamCurrentScore(teamID) / 10)
-              "
-            />
-          </div>
-          <ion-popover trigger="level-indicator" trigger-action="click">
-            <ion-content class="ion-padding"
-              >Indication of team level and progress bar to next
-              level</ion-content
-            >
-          </ion-popover>
-        </ion-col>
-
-        <ion-col
-          id="growth-indicator"
-          size="auto"
-          v-show="statusStore.teamActiveScoreGrowth(teamID)"
-        >
-          <div style="float: left">
-            <ion-icon :icon="arrowUpCircleOutline" size="large"></ion-icon>
-          </div>
-          <div style="float: left; width: auto" :class="{ pump: animate }">
-            <div style="font-size: 20px; width: 100%; text-align: center">
-              {{
-                Math.round(
-                  statusStore.teamActiveScoreGrowth(teamID)
-                    ? statusStore.teamActiveScoreGrowth(teamID)
-                    : 0
-                )
-              }}%
-            </div>
-          </div>
-          <ion-popover trigger="growth-indicator" trigger-action="click">
-            <ion-content class="ion-padding"
-              >Score growth with respect to previous survey</ion-content
-            >
-          </ion-popover>
-        </ion-col>
-
-        <ion-col id="rank-indicator">
-          <div style="float: left; width: auto">
-            <ion-icon
-              :icon="cellularOutline"
-              size="large"
-              style="transform: rotate(90deg)"
-            ></ion-icon>
-          </div>
-          <div style="float: left">
-            <div style="font-size: 20px; width: 100%; text-align: center">
-              Top
-              {{ Math.max(Math.round(statusStore.teamActiveRank(teamID)), 1) }}%
-            </div>
-          </div>
-          <ion-popover trigger="rank-indicator" trigger-action="click">
-            <ion-content class="ion-padding"
-              >Rank with respect to all other teams</ion-content
-            >
-          </ion-popover>
-        </ion-col>
-      </ion-row>
-    </div>
-  </ion-grid>
-
-  <!--ion-item v-show="surveysStore.activeSurveys.length > 0" @click="() => router.push({name: 'surveys'})">
-        <ion-badge slot="start">{{ surveysStore.activeSurveys.length }}</ion-badge>
-        <ion-label>Open surveys</ion-label>
-    </ion-item-->
+  <ion-header>
+      <ion-toolbar>
+          <ion-grid>
+              <ion-row>
+                  <ion-col size="2">
+                      <ion-buttons>
+                          <ion-button>
+                              <ion-icon :icon="peopleSharp"></ion-icon>
+                          </ion-button>
+                      </ion-buttons>
+                  </ion-col>
+                  <ion-col size="7">
+                      <div>
+                          <ion-select  
+                          interface="popover" placeholder="Team dep..." class="header-select">
+                              <!-- Your options here -->
+                              <ion-select-option value="team1">Team 1 department </ion-select-option>
+                              <ion-select-option value="team2">Team 2 department</ion-select-option>
+                              <ion-select-option value="team3">Team 3 department</ion-select-option>
+                              <ion-select-option value="team4">Team 4 department</ion-select-option>
+                              <!-- Additional options -->
+                          </ion-select>    
+                      </div>
+                  </ion-col>
+                  <ion-col size="3">
+                      <ion-buttons>
+                          <ion-button>
+                              <ion-icon class="profile-Icon-style" :icon="personCircleSharp"></ion-icon>
+                          </ion-button>
+                      </ion-buttons>
+                  </ion-col>
+              </ion-row>
+          </ion-grid>
+      </ion-toolbar>
+  </ion-header>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
+import {
+  IonHeader, IonToolbar, IonButtons, IonButton, IonIcon,
+  IonGrid, IonRow, IonCol, IonSelect, IonSelectOption
+} from '@ionic/vue';
+import { peopleSharp, personCircleSharp } from 'ionicons/icons';
 import { ref, watch } from "vue";
-import {
-  IonTitle,
-  IonIcon,
-  IonProgressBar,
-  IonSelect,
-  IonSelectOption,
-  IonPopover,
-  IonContent,
-  IonGrid,
-  IonRow,
-  IonCol,
-} from "@ionic/vue";
-import {
-  arrowUpCircleOutline,
-  prismOutline,
-  menuOutline,
-  cellularOutline,
-} from "ionicons/icons";
 import router from "@/router/index";
-import { useUserStore } from "@/stores/user";
-import { useStatusStore } from "@/stores/status";
-import { useTeamStore } from "@/stores/teams";
-import { fetchCurrent, teamID, team } from "@/stores/current";
+export default {
+  name: 'HeaderComponent',
+  components: {
+      IonHeader, IonToolbar, IonButtons, IonButton, IonIcon,
+      IonGrid, IonRow, IonCol, IonSelect, IonSelectOption
+  },
+  setup() {
+      return {
+          peopleSharp,
+          personCircleSharp
+      };
+  }
+};
 
-const userStore = useUserStore();
-const teamStore = useTeamStore();
-const statusStore = useStatusStore();
 
-await Promise.all([
-  userStore.fetch(),
-  statusStore.fetch(),
-  teamStore.fetch(),
-  fetchCurrent(),
-]);
+
+
+
 const buffer = 1.0;
 
-const teamSelected = ref(teamID.value);
-console.log(teamSelected.value);
 
-function changeTeam() {
-  if (teamSelected.value == "add") {
-    router.push({ name: "addteam" });
-  } else {
-    userStore.settings.update_value("last_team_used", teamSelected.value);
-  }
-}
 
-const animate = ref(false);
-function animateIncrease() {
-  animate.value = true;
-  setTimeout(() => {
-    animate.value = false;
-  }, 820);
-}
 </script>
 
 <style scoped>
+ion-header {
+  --background: #333;
+  --ion-toolbar-background: #333;
+  --ion-color-base: #333;
+  height: 50px;
+  /* Set the height of the header */
+}
+
+ion-toolbar {
+  --padding-top: 0;
+  --padding-bottom: 0;
+  min-height: 50px;
+  /* Ensure the toolbar is also 50px in height */
+  align-items: center;
+  /* Vertically center the toolbar content */
+}
+
+.header-select {
+  --padding-start: 6px;
+  --padding-end: 0px;
+  --border-radius: 8px;
+  --background: #4f4f4f;
+  --placeholder-color: #bcbcbc;
+  --placeholder-opacity: 1;
+  --color: #fff;
+  height: 5px;
+  width: 120px;
+  /* Adjust the height of the select */
+  --border: none;
+  --inner-padding-top: 0;
+  --inner-padding-bottom: 0;
+  --padding-top: 0;
+  --padding-bottom: 0;
+}
+
+ion-select {
+  max-height: 50px;
+  justify-content: center;
+  align-items: center;
+}
+
+ion-select::part(text) {
+  padding-top: 0;
+  /* Align the text to the top */
+  padding-bottom: 0;
+  /* No padding at the bottom */
+}
+
+ion-select::part(icon) {
+  margin-top: -8px;
+  /* Adjust the icon position */
+}
+
+ion-icon {
+  font-size: 24px;
+  color: #fff;
+}
+
+ion-button {
+  --background: transparent;
+  --background-activated: rgba(255, 255, 255, 0.1);
+  --background-hover: rgba(255, 255, 255, 0.1);
+  --ripple-color: rgba(255, 255, 255, 0.1);
+}
+.profile-Icon-style {
+  padding-left: 40px;
+}
+
 .pump {
-  animation: pump 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97) both;
+animation: pump 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97) both;
 }
 
 @keyframes pump {
-  50% {
-    transform: scale(1.4, 1.4);
-  }
+50% {
+  transform: scale(1.4, 1.4);
 }
+}
+/* You may need to adjust the sizes or padding of the columns depending on your content */
 </style>
