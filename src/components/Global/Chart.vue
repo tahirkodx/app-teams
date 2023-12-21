d2287bb0d2915ae0dc27f41d8c6bff9500d87402
adilameeqKOD <adilameeq@kodxsystem.com>
2023-12-21 15:57:00 +0500

    feat: graph

--- src/components/Global/Chart.vue
+++ src/components/Global/Chart.vue
@@ -1,31 +1,68 @@
 <template>
-  <div v-if="chartData && chartData?.labels && chartData?.datasets">
-    <radar-chart :chart-data="chartData" :options="chartOptions" />
-  </div>
+  <Radar :data="chartData" :options="chartOptions" />
 </template>
 
-<script lang="ts">
-import { Radar } from 'vue-chartjs';
-import { defineComponent, ref, onMounted } from 'vue';
+<script setup lang="ts">
+import { defineComponent, ref } from 'vue';
+import { Radar } from 'vue-chartjs'
+import {
+  Chart as ChartJS,
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend
+} from 'chart.js'
 
-export default defineComponent({
-  components: {
-    RadarChart: Radar
-  },
-  setup() {
-    const chartData = ref({
-      // Your initial chart data goes here
-    });
+// Register the necessary Chart.js components
+ChartJS.register(
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
 
-    const chartOptions = ref({
-      // Your chart options go here
-    });
+)
 
-    onMounted(async () => {
-      // If you're fetching data, do it here and then update chartData.value
-    });
+// Assuming chartConfig.js exports an object with `data` and `options` properties
+// Make sure to adjust the import path to your chartConfig.js file
+import { options } from './chartConfig';
 
-    return { chartData, chartOptions };
+// Reactive references for chart data and options
+// const chartData = ref(data);
+const chartOptions = ref(options);
+const chartData = {
+    labels: [
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7'
+    ],
+    datasets: [
+      {
+        label: 'My Second dataset',
+        backgroundColor: 'rgba(165,206,61,1)',
+        borderColor: 'rgba(165,206,61,1)',
+        pointBackgroundColor: 'rgba(165,206,61,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(165,206,61,1)',
+        data: [28, 48, 40, 19, 96, 27, 100]
+      }
+    ]
   }
-});
 </script>
+<style scoped>
+.chart-canvas {
+  display: flex;
+  align-items: flex-start; /* Aligns items to the start of the container's cross axis */
+  box-sizing: border-box;
+  width: 390px; /* Adjust as needed */
+  height: 793.333px; /* Adjust as needed */
+  /* Additional styles */
+}
+</style>
---
+++ src/components/Global/chartConfig.ts
@@ -0,0 +1,46 @@
+export const options = {
+    responsive: true,
+    layout: {
+      padding: {
+        top: 0 // Set to 0 or adjust as needed
+      }
+    },
+  scales: {
+    r: {
+      grid: {
+        color: 'rgba(255, 255, 255, 0.5)' // Lighter grid lines against the dark background
+      },
+      angleLines: {
+        color: 'rgba(255, 255, 255, 0.5)' // Lighter angle lines
+      },
+      pointLabels: {
+        color: '#fff' // Text color for the labels
+      },
+      ticks: {
+        display: false // This will hide the tick labels such as 100, 90, 80, etc.
+      },
+      
+    }
+  },
+  plugins: {
+    legend: {
+      labels: {
+        color: '#fff' // Color of the legend labels
+      }
+    },
+    tooltip: {
+      // Tooltip configuration, if needed
+    }
+  },
+  elements: {
+    line: {
+      borderWidth: 2 // Width of the radar line
+    },
+    point: {
+      radius: 3 // Radius of the points on the radar
+    }
+  },
+  maintainAspectRatio: false // Set to false to allow custom sizes
+
+  }
+  
