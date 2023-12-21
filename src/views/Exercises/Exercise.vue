83b06b0ce4c0e18033694e4054e0f996c36febf4
haseebkodx <haseeb@kodxsystem.com>
2023-12-22 02:58:47 +0500

    Exercise Screen is done

--- src/views/Exercises/Exercise.vue
+++ src/views/Exercises/Exercise.vue
@@ -1,23 +1,25 @@
 <template>
     <ion-page>
         <ion-header>
-            <ion-toolbar>
-                <ion-title>Practice</ion-title>
-            </ion-toolbar>
+            <score />
         </ion-header>
         <ion-content :fullscreen="true">
-            <ion-reorder-group @ion-item-reorder="doReorder" disabled="false">
+            <ion-reorder-group @ionItemReorder="doReorder" :disabled="false">
                 <ion-card v-for="(item, index) in practiceItems" :key="index" class="practice-card">
                     <div class="card-content">
-                        <ion-reorder slot="start"></ion-reorder>
-                        <ion-thumbnail slot="start">
-                            <img :src="item.imgSrc">
+                        <ion-reorder slot="end">
+                            <img src="/src/pictures/threeDot.svg" class="drag-handle-icon" />
+                        </ion-reorder>
+
+                        <ion-thumbnail slot="end">
+                            <img :src="item.imgSrc" />
                         </ion-thumbnail>
-                        <ion-label>{{ item.title }}</ion-label>
-                        <ion-icon :icon="chevronForward" slot="end" />
-                    </div>
-                    <div class="progress-container">
-                        <ion-progress-bar :value="item.progress" color="success"></ion-progress-bar>
+                        <div class="title-progress-container">
+                            <ion-label>{{ item.title }}</ion-label>
+                            <ion-progress-bar :value="item.progress" color="success"
+                                class="progress-bar"></ion-progress-bar>
+                        </div>
+                        <ion-icon :icon="chevronForwardOutline" slot="end" class="arrow-icon" />
                     </div>
                 </ion-card>
             </ion-reorder-group>
@@ -26,7 +28,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref } from "vue";
 import {
     IonPage,
     IonHeader,
@@ -34,35 +36,50 @@ import {
     IonTitle,
     IonContent,
     IonCard,
-    IonItem,
     IonLabel,
     IonThumbnail,
     IonProgressBar,
     IonReorderGroup,
     IonReorder,
-    IonIcon
-} from '@ionic/vue';
-import { chevronForward } from 'ionicons/icons';
-import type { ReorderEventDetail } from '@ionic/core';
-
-// Sample items, replace 'imgSrc' with your actual image paths
-
+    IonIcon,
+} from "@ionic/vue";
+import { chevronForwardOutline, pizza } from "ionicons/icons";
+import type { ReorderEventDetail } from "@ionic/core";
+import score from "@/components/Header/Header.vue";
 
 const practiceItems = ref([
-    { title: "Communication", imgSrc: "/path/to/your/image.png",subtitle: "20", progress: 0.5 },
-    { title: "Celebrate and Collaborate", imgSrc: "/path/to/your/image.png",subtitle: "", progress: 0.3 },
-    { title: "Effective Feedback", imgSrc: "/path/to/your/image.png",subtitle: "", progress: 0.7 },
-    { title: "Active Listening", imgSrc: "/path/to/your/image.png",subtitle: "", progress: 0.2 },
+    {
+        title: "Communication",
+        imgSrc: "/src/pictures/Illustration 6.svg",
+        progress: 0.5,
+    },
+    {
+        title: "Celebrate and Collaborate",
+        imgSrc: "/src/pictures/Illustration 7.svg",
+        progress: 0.3,
+    },
+    {
+        title: "Effective Feedback",
+        imgSrc: "/src/pictures/Illustration 5.svg",
+        progress: 0.7,
+    },
+    {
+        title: "Active Listening",
+        imgSrc: "/src/pictures/Illustration 4.svg",
+        progress: 0.2,
+    },
     // ... other items
 ]);
+
 const doReorder = (event: CustomEvent<ReorderEventDetail>) => {
     const fromIndex = event.detail.from;
     const toIndex = event.detail.to;
-    if (fromIndex !== toIndex) {
-        const itemToMove = practiceItems.value.splice(fromIndex, 1)[0];
-        practiceItems.value.splice(toIndex, 0, itemToMove);
-    }
-    event.detail.complete(practiceItems.value);
+    practiceItems.value.splice(
+        toIndex,
+        0,
+        practiceItems.value.splice(fromIndex, 1)[0]
+    );
+    event.detail.complete();
 };
 </script>
 
@@ -70,55 +87,68 @@ const doReorder = (event: CustomEvent<ReorderEventDetail>) => {
 .practice-card {
     margin-bottom: 8px;
     --ion-item-background: transparent;
-    --ion-item-padding: 0;
     --background: transparent;
     box-shadow: none;
     /* Remove shadow if present */
+    display: flex;
+    flex-direction: column;
+    padding: 0px 20px 4px 20px;
 }
 
 .card-content {
     display: flex;
     align-items: center;
+    padding: 16px;
+    /* Add padding if needed */
+    border-radius: 5px;
+    background: #ffffff;
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
 }
 
-ion-item {
-    --padding-start: 0;
-    --padding-end: 0;
-    --inner-padding-end: 0;
-    --inner-padding-start: 0;
+.title-progress-container {
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+}
+
+ion-label {
+    font-size: 1rem;
+    margin-bottom: 4px;
+    /* Space between title and progress bar */
+}
+
+.progress-bar {
+    --progress-background: #e6e0e9;
+    /* --buffer-background: transparent; */
+    --progress-color: #a5ce3e;
+    width: 50%;
+    /* Adjust width as needed */
+}
+
+.arrow-icon {
+    font-size: 1.5em;
+    margin-left: auto;
+    color: #303030;
+    /* Ensures the arrow icon is aligned to the right */
 }
 
 ion-thumbnail {
     --border-radius: 0;
-    --size: 48px;
+    --size: 55px;
     /* Adjust the size based on your design */
     margin-right: 8px;
+    width: 105px;
+    height: 60px;
     /* Spacing between image and title */
-}
-
-ion-label {
-    flex-grow: 1;
-    font-size: 1rem;
-    padding: 16px 0;
-}
-
-.progress-container {
-    width: 100%;
-    /* Progress bar full width */
-    padding: 0 16px;
-    /* Padding on sides, adjust as needed */
-}
-
-ion-progress-bar {
-    height: 4px;
-    /* Height of progress bar, adjust as needed */
-    --buffer-background: #e4e4e4;
 }
 
 ion-reorder {
     color: var(--ion-color-medium);
 }
 
-ion-icon {
-    font-size: 1.5em;
-}</style>
+.drag-handle-icon {
+    width: 15px;
+    height: 20px;
+    margin-right: 10px;
+}
+</style>
