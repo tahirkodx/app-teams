e31a897f8cbdcce0c71158fe12469dbc115e1e08
haseeb <haseeb@kodxsystem.com>
2024-04-29 17:52:33 +0500

    fix changes

---
+++ src/views/Passwordchanged/ChangePassword.vue
@@ -0,0 +1,70 @@
+<template>
+    <ion-page>
+        <Header is-back-button />
+        <ion-content>
+            <ion-row>
+                <ion-col class="ion-padding">
+                    <div class="custom-container ion-padding">
+                        <div class=" custom-container ion-text-center ion-margin-bottom">
+                        <ion-text class="font-lg">Change Password?</ion-text>
+                        <ion-text class="font-sm">We will send you a link to reset password</ion-text>  
+                        </div>
+                        <ion-input
+                          fill="outline"
+                          placeholder="Enter email:*"
+                           class="custom-input ion-margin-bottom"
+                           ></ion-input>
+                         <ion-button
+                        expand="block"
+                        class="ion-padding-vertical"
+                        router-link="/resetOtp"
+                        >SEND LINK</ion-button
+                        >
+                  </div>
+                </ion-col>
+            </ion-row>
+        </ion-content>
+    </ion-page>
+</template>
+
+<script setup lang="ts">
+import {
+    IonPage,
+    IonContent,
+    IonRow,
+    IonCol,
+    IonIcon,
+    IonText
+} from "@ionic/vue";
+import Header from "@/components/Header/Header.vue";
+
+</script>
+<style scopped>
+.custom-container {
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+}
+
+.font-lg {
+    color: #000;
+    font-size: 22px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 28px;
+}
+.custom-input {
+  --background: #f2f2f2;
+  --border-width: 0;
+  --border-radius: 5px;
+  margin-top: 10px;
+}
+.font-sm {
+    color: #808080;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 20px;
+    letter-spacing: 0.1px;
+}
+</style>
---
+++ src/views/Passwordchanged/ResetOtp.vue
@@ -0,0 +1,115 @@
+<template>
+    <ion-page>
+      <Header is-back-button />
+      <ion-content>
+        <ion-row>
+          <ion-col class="ion-padding">
+            <div class="custom-container ion-padding">
+              <div class="custom-container ion-text-center ion-margin-bottom">
+                <ion-text class="font-lg">Reset new password</ion-text>
+                <ion-text class="font-sm">Please enter the OTP code sent to email address</ion-text>
+                <ion-text class="font-sm text-blue">(danielrussel @google.com)</ion-text>
+              </div>
+              <v-otp-input
+                ref="otpInput"
+                input-classes="otp-input"
+                separator=" "
+                inputType="letter-numeric"
+                :num-inputs="6"
+                v-model:value="bindValue"
+                :should-auto-focus="true"
+                :should-focus-order="true"
+                @on-change="handleOnChange"
+                @on-complete="handleOnComplete"
+              />
+              <ion-button expand="block" class="ion-padding-vertical" router-link="/resetPassword">VERIFY EMAIL</ion-button>
+            </div>
+          </ion-col>
+        </ion-row>
+      </ion-content>
+    </ion-page>
+  </template>
+  
+  <script setup lang="ts">
+  import { ref } from 'vue';
+  import { IonPage, IonContent, IonRow, IonCol, IonText, IonButton } from '@ionic/vue';
+  import Header from '@/components/Header/Header.vue';
+  import VOtpInput from 'vue3-otp-input';
+  
+  const otpInput = ref<InstanceType<typeof VOtpInput> | null>(null);
+  const bindValue = ref('');
+  
+  const handleOnComplete = (value: string) => {
+    console.log('OTP completed: ', value);
+  };
+  
+  const handleOnChange = (value: string) => {
+    console.log('OTP changed: ', value);
+  };
+  
+
+  
+  </script>
+  
+  <style>
+  .custom-container {
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+  }
+  
+  .font-lg {
+    color: #000;
+    font-size: 22px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 28px;
+  }
+  
+  .custom-input {
+    --background: #f2f2f2;
+    --border-width: 0;
+    --border-radius: 5px;
+    margin-top: 10px;
+  }
+  
+  .font-sm {
+    color: #808080;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 20px;
+    letter-spacing: 0.1px;
+  }
+.text-blue{
+    color: #0D99FF;
+}
+  .otp-input {
+  width: 50px;
+  height: 40px;
+  padding: 12px 10px;
+  margin: 0 4px;
+  font-size: 20px;
+  border:none;
+  border-radius: 5px;
+  background: #F2F2F2;
+  text-align: center;
+  outline: none;
+}
+/* Background colour of an input field with value */
+/* .otp-input.is-complete {
+  background-color: #e4e4e4;
+}
+.otp-input::-webkit-inner-spin-button,
+.otp-input::-webkit-outer-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+input::placeholder {
+  font-size: 15px;
+  text-align: center;
+  font-weight: 600;
+} */
+
+  </style>
+  
